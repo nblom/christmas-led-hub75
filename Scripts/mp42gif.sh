f3f3ba612da1d3e4991bc74c4acd66c4189375b5
Niklas Blomdalen <niklas.blomdalen@lopnet.se>
2025-10-31 13:59:11 +0100

    color grading

--- Scripts/mp42gif.sh
+++ Scripts/mp42gif.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# Convert all MP4 files in Masters/*/*.mp4 to GIFs in Unfinished/ if they don't already exist in Unfinished/ or Finished/
+# Convert all MP4 files in Masters/*/*.mp4 to GIFs in Converted/ if they don't already exist in Converted/ or Finished/
 # Uses ffmpeg to perform the conversion and git to track the new files
 # Call from base directory, ie ./Scripts/mp42gif.sh
 
 for mp4file in Masters/*/*.mp4; do
     giffile=$(basename "${mp4file%.mp4}.gif")
-    if [ ! -f "Unfinished/$giffile" ] && [ ! -f "Finished/$giffile" ]; then
-        ffmpeg -i "$mp4file" -y -vf 'fps=25,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse,scale=128:64:flags=lanczos' -loop 0 "Unfinished/$giffile"
-        git add "Unfinished/$giffile"
-        git commit "Unfinished/$giffile" -m "Add GIF for $(basename "$mp4file")"
+    if [ ! -f "Converted/$giffile" ]; then
+        #ffmpeg -i input.mp4 -vf "curves=all='0/0 0.1/0 0.9/1 1/1'" -c:a copy output.mp4
+        ffmpeg -i "$mp4file" -y -vf "fps=25,curves=all='0/0 0.1/0 0.9/1 1/1',split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse,scale=128:64:flags=lanczos" -loop 0 "Converted/$giffile"
+        git add "Converted/$giffile"
+        git commit "Converted/$giffile" -m "Add converted GIF for $(basename "$mp4file")"
     fi
 done
