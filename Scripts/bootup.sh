7f271b37c7f4e13dda76d8ceea8b38f4eb616358
Niklas Blomdalen <niklas.blomdalen@lopnet.se>
2025-11-12 16:17:48 +0100

    Added bootup with IP

---
+++ Scripts/bootup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+# maybe better with wpa_cli -i wlan0 status
+
+(echo "Connect to:"; $(iwgetid -r); echo $(ip --brief -4 address show scope global | awk '{print $3}'); sleep 60) | \
+../rpi-rgb-led-matrix/examples-api-use/text-example \
+--led-limit-refresh=200 \
+--led-cols 64 \
+--led-rows 32 \
+--led-slowdown-gpio=4 \
+--led-chain=2 \
+--led-parallel=2 \
+--led-brightness=50 \
+-f ./rpi-rgb-led-matrix/fonts/6x10.bdf
