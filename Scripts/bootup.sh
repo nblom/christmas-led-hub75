c301636cbf37597892a8c614f36e885752dff10a
Niklas Blomdalen <niklas.blomdalen@lopnet.se>
2025-11-22 13:06:21 +0100

    Bootup message

--- Scripts/bootup.sh
+++ Scripts/bootup.sh
@@ -2,7 +2,7 @@
 
 # maybe better with wpa_cli -i wlan0 status
 
-(echo "Connect to:"; $(iwgetid -r); echo $(ip --brief -4 address show scope global | awk '{print $3}'); sleep 60) | \
+(echo "Connect to: " $(iwgetid -r); echo $(ip --brief -4 address show scope global | awk '{print $3}'); sleep 60) | \
 ../rpi-rgb-led-matrix/examples-api-use/text-example \
 --led-limit-refresh=200 \
 --led-cols 64 \
@@ -11,4 +11,4 @@
 --led-chain=2 \
 --led-parallel=2 \
 --led-brightness=50 \
--f ./rpi-rgb-led-matrix/fonts/6x10.bdf
+-f ../rpi-rgb-led-matrix/fonts/6x10.bdf
